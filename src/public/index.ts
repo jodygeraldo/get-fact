84259d554e769f0d042d5e9b829c0fd40d0a6bdd
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-26 18:34:09 +0700

    feat(build): only rebuild css file if file changed

--- src/public/index.ts
+++ src/public/index.ts
@@ -5,21 +5,10 @@ import { emptyDirSync, init, transform } from "../deps.ts";
 await init();
 
 const publicBuildPath = `./public/build`;
-const htmlPath = `./src/public/index.html`;
 const cssPath = `./src/public/styles/styles.css`;
-const html = await Deno.readTextFile(htmlPath);
+const htmlPath = `./src/public/index.html`;
 const css = await Deno.readTextFile(cssPath);
-
-emptyDirSync(`${publicBuildPath}/styles`);
-
-const randomHash = randomAlphanumericString(8);
-
-const processedHtml = html.replace(
-  "[GENERATED-CSS-STYLES]",
-  `styles-${randomHash}`,
-);
-
-await Deno.writeTextFile(`${publicBuildPath}/index.html`, processedHtml);
+const html = await Deno.readTextFile(htmlPath);
 
 const { code } = transform({
   filename: "styles.css",
@@ -29,7 +18,35 @@ const { code } = transform({
 
 const processedCss = new TextDecoder().decode(code);
 
-await Deno.writeTextFile(
-  `${publicBuildPath}/styles/styles-${randomHash}.css`,
-  processedCss,
+const cssFiles: string[] = [];
+
+for await (const dirEntry of Deno.readDirSync(`${publicBuildPath}/styles`)) {
+  if (dirEntry.isFile) {
+    cssFiles.push(dirEntry.name);
+  }
+}
+
+const oldCss = Deno.readTextFileSync(
+  `${publicBuildPath}/styles/${cssFiles[0]}`,
 );
+console.log(oldCss);
+
+if (oldCss === processedCss) {
+  console.log("No change in css");
+} else {
+  emptyDirSync(`${publicBuildPath}/styles`);
+
+  const randomHash = randomAlphanumericString(8);
+
+  const processedHtml = html.replace(
+    "[GENERATED-CSS-STYLES]",
+    `styles-${randomHash}`,
+  );
+
+  await Deno.writeTextFile(`${publicBuildPath}/index.html`, processedHtml);
+
+  await Deno.writeTextFile(
+    `${publicBuildPath}/styles/styles-${randomHash}.css`,
+    processedCss,
+  );
+}
